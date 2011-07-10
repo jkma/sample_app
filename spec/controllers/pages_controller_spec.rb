8e2e11e1b2984fce9842a4588e518db6fdcc5ee4
jkma <notKarel@hotmail.com>
2011-07-10 12:01:51 +0800

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -35,4 +35,14 @@ render_views
   end 
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+	get 'help'
+	response.should be_success
+    end
+  it "should have the right title" do
+	get 'help'
+	response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+  end
+  end
 end
